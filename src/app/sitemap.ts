eda8bc1488843946e043cfb5ee2694f66944e64e
Moaz El Gandy <zezogameing1234@gmail.com>
2025-08-03 16:05:59 +0300

    feat: update branding from Marketopia to Narmertex in SEO, sitemap, and robots files

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://marketopia.com",
+      url: "https://narmertex.com",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://marketopia.com/about",
+      url: "https://narmertex.com",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://marketopia.com/products",
+      url: "https://narmertex.com/products",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.5,
